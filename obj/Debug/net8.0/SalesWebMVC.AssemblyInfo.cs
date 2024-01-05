7f13227cc4841b35612f3c9b03ea1c1f11475505
Wesley Brianez <wesleybrianez@gmail.com>
2024-01-05 01:28:19 -0300

    first MVC: Department

--- obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11d0a1cdb294baaf089462340ae2eace9f76f7cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
